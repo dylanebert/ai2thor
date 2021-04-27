a5fc59155f97b78d8edc4952f031c1d197b70510
dylan <dylan.ebert@gmail.com>
2021-04-27 15:11:58 -0400

    .

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from ai2thor.controller import Controller
 import pandas as pd
+import random
+
+
 controller = Controller(scene="FloorPlan10")
 controller.step('PausePhysicsAutoSim')
-data = []
-for obj in controller.last_event.metadata['objects']:
-    if obj['pickupable']:
-        name = obj['name']
-        pos = obj['position']
-        rot = obj['rotation']
-        print(name, pos, rot)
+
+
+def get_random_pickupable():
+    candidates = []
+    for obj in controller.last_event.metadata['objects']:
+        if obj['pickupable']:
+            candidates.append(obj['objectId'])
+    return random.choice(candidates)
+
+
+obj = get_random_pickupable()
+print(obj)
