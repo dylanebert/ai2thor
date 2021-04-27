fd248298cf3029515b9072425649c9cb54464642
dylan <dylan.ebert@gmail.com>
2021-04-27 14:38:11 -0400

    .

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ai2thor.controller import Controller
 controller = Controller(scene="FloorPlan10")
-event = controller.step(action="RotateRight")
-metadata = event.metadata
-print(event, event.metadata.keys())
+controller.step('PausePhysicsAutoSim')
+for obj in controller.last_event.metadata['objects']:
+    print(obj)
