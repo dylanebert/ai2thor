c69b9929a2e063bbe97c9651472bcc98afe1b3e7
dylan <dylan.ebert@gmail.com>
2021-04-27 14:48:13 -0400

    .

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from ai2thor.controller import Controller
 controller = Controller(scene="FloorPlan10")
 controller.step('PausePhysicsAutoSim')
 for obj in controller.last_event.metadata['objects']:
-    print(obj['name'])
+    print(obj['name'], obj['pickupable'])
