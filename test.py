a3d6e2c4d8ebbab73b45306dfb95bb95646fb85b
dylan <dylan.ebert@gmail.com>
2021-04-27 15:21:36 -0400

    .

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from ai2thor.controller import Controller
+import numpy as np
 import pandas as pd
 import random
 
@@ -11,9 +12,26 @@ def get_random_pickupable():
     candidates = []
     for obj in controller.last_event.metadata['objects']:
         if obj['pickupable']:
-            candidates.append(obj['objectId'])
+            candidates.append(obj)
     return random.choice(candidates)
 
 
+def update_to_current_frame(obj):
+    name = obj['name']
+    for candidate in controller.last_event.metadata['objects']:
+        if candidate['name'] == name:
+            return candidate
+
+
 obj = get_random_pickupable()
-print(obj)
+dir = np.random.rand(3)
+dir /= np.linalg.norm(dir)
+print(obj['position'])
+controller.step(
+    action='PushObject',
+    objectId=obj['objectId'],
+    moveMagnitude='100',
+    forceAction=True
+)
+obj = update_to_current_frame(obj)
+print(obj['position'])
